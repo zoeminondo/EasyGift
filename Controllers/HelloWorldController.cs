95bf43957930cdb3c2f2f217b29685be7677b895
Zoé Minondo <zoeminondo@gmail.com>
2021-12-11 11:11:26 +0100

    commit

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -18,9 +18,12 @@ namespace EasyGift.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
-            {
-                return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-            }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            
+        }
     }
 }
