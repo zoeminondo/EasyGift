0dcffb4aa701dc7e3ddabf19e042e754ffc6b52c
Zoé Minondo <zoeminondo@gmail.com>
2021-12-19 23:08:16 +0100

    commit

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -2,9 +2,19 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+
 
 namespace EasyGift.Controllers
 {
+  
+
     public class HelloWorldController : Controller
     {
         // 
@@ -20,9 +30,31 @@ namespace EasyGift.Controllers
 
         public IActionResult Welcome(string name, int numTimes = 1)
         {
-            ViewData["Message"] = "Hello " + name;
+            ViewData["Message"] = "Hello evrybody " + name;
             ViewData["NumTimes"] = numTimes;
             return View();
+            
+        }
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            
+            if (file == null){
+
+                    return View();
+            }
+
+            else{
+                
+                string adresseEasyGift = Directory.GetCurrentDirectory();
+                using var image = Image.Load(file.OpenReadStream());
+                image.Mutate(x => x.Resize(256, 256));
+                // Il faut changer le nom de la photo
+                image.Save(adresseEasyGift+"/wwwroot/images/filename.jpg");
+                
+                return Ok();
+
+            }
             
         }
     }
